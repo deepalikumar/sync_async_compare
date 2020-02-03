22f0f337d321c0be3adad9259f36b348698e9dea
deepali kumar <d@acnt.me>
2020-02-03 16:09:33 +0530

    setup extensions

--- flask_sync/app.py
+++ flask_sync/app.py
@@ -4,14 +4,18 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
+
 def setup_extensions(app):
     db.init_app(app)
     Migrate(app, db)
+
 
 def create_app(override_settings=None):
     app = Flask(__name__)
     app.config.from_object("settings")
     if override_settings:
         app.config.update(**override_settings)
-    
+
+    setup_extensions(app)
+
     return app
