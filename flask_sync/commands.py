1cb17d468af2554537ea60aae8951a0dea879d99
Deepali Kumar <d@acnt.me>
2020-02-08 02:34:31 +0530

    seed data

--- flask_sync/commands.py
+++ flask_sync/commands.py
@@ -1,9 +1,8 @@
-import click
 from flask.cli import AppGroup
 from faker import Faker
 
 from app import db
-from blog.models import User, Post, Comment, Category
+from blog.models import User, Post, Comment, Category, PostCategory
 
 
 data_cli = AppGroup("data")
@@ -14,14 +13,18 @@ Faker.seed(4321234)
 @data_cli.command("seed")
 def seed():
     """Seeds test data"""
+    PostCategory.query.delete()
     Category.query.delete()
+    Comment.query.delete()
+    Post.query.delete()
+    User.query.delete()
+
     programming = Category(name="programming")
     finance = Category(name="finance")
     travel = Category(name="travel")
     general = Category(name="general")
     categories = [programming, finance, travel, general]
 
-    Comment.query.delete()
     comments = []
     for _ in range(1000):
         comments.append(
@@ -31,21 +34,21 @@ def seed():
             )
         )
 
-    User.query.delete()
-    for _ in range(10):
+    for _ in range(100):
         user = User(username=fake.user_name(), email=fake.email())
-        for _ in range(10):
+        for _ in range(100):
             post = Post(
                 title=fake.bs(),
                 body=fake.paragraph(nb_sentences=30, variable_nb_sentences=True),
             )
-            post.comments = fake.random_choices(
-                comments, length=fake.random_int(min=0, max=100)
+            post.comments = fake.random_elements(
+                comments, length=fake.random_int(0, 100), unique=True
             )
-            post.categories = fake.random_choices(
-                categories, length=fake.random_int(1, 4)
+            post.categories = fake.random_elements(
+                categories, length=fake.random_int(1, 4), unique=True
             )
             user.posts.append(post)
         db.session.add(user)
 
     db.session.commit()
+    db.session.close_all()
