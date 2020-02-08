103ed6a40610d92d87bab152af0e6769c3cf4e61
Deepali Kumar <d@acnt.me>
2020-02-09 01:15:26 +0530

    Changes to sarlette startup

--- starlette_async/app.py
+++ starlette_async/app.py
@@ -1,18 +1,51 @@
 import sys
+
 import sqlalchemy
+import databases
 
 from starlette.applications import Starlette
-from starlette.config import Config
+from starlette.responses import UJSONResponse
+from starlette.exceptions import HTTPException
 from starlette.routing import Mount
 
-config = Config(".env")
-app = Starlette(debug=config("debug", bool, False))
+import settings
+
+app = None
+database = None
+metadata = sqlalchemy.MetaData()
 
 
 def setup_routes():
     import blog.views
 
 
+async def on_startup():
+    await database.connect()
+
+
+async def on_shutdown():
+    await database.disconnect()
+
+
+async def http_exception(request, exc):
+    return UJSONResponse({"errors": [exc.detail]}, status_code=exc.status_code)
+
+
 def create_app():
     sys.path.append(".")
-    setup_routes()
+    global app, database
+    if settings.TESTING:
+        database = databases.Database(settings.TEST_DATABASE_URL, force_rollback=True)
+    else:
+        database = databases.Database(settings.DATABASE_URL)
+
+    app = Starlette(
+        debug=settings.DEBUG,
+        on_startup=[database.connect],
+        on_shutdown=[database.disconnect],
+        exception_handlers={HTTPException: http_exception},
+    )
+    return app
+
+
+create_app()
