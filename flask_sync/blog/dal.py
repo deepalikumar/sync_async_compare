b8420eb6e2436cea99ffe41f89547fb908796991
Deepali Kumar <d@acnt.me>
2020-02-09 01:17:03 +0530

    Changed dal for joinedload

--- flask_sync/blog/dal.py
+++ flask_sync/blog/dal.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import joinedload
 from .models import Post, User
 
 
@@ -7,4 +8,10 @@ def get_user_posts(user_id, *, category_id=None, limit=10, offset=0):
         user_posts = user_posts.filter(Post.category_id == category_id)
     limit = limit if 1 <= limit <= 50 else 10
     offset = offset if offset >= 0 else 0
-    return user_posts.offset(offset).limit(limit)
+    return (
+        user_posts.options(joinedload(Post.comments))
+        .options(joinedload(Post.owner))
+        .options(joinedload(Post.categories))
+        .offset(offset)
+        .limit(limit)
+    )
