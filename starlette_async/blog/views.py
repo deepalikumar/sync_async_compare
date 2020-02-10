112deb0bc40baedcc4d61f2c91ea7f3546f73188
Deepali Kumar <d@acnt.me>
2020-02-10 15:58:50 +0530

    get user posts and return it

--- starlette_async/blog/views.py
+++ starlette_async/blog/views.py
@@ -1,8 +1,14 @@
 from starlette.responses import UJSONResponse
 from starlette.endpoints import HTTPEndpoint
 from starlette.routing import Route
+from spectree import Response
+
+from app import api
+from .dal import get_user_posts
+from .schemas import PostListSchema
 
 
-class UserListEndpoint(HTTPEndpoint):
+class UserPostListEndpoint(HTTPEndpoint):
     async def get(self, request):
-        return UJSONResponse({"hello": "world"})
+        user_id = request.path_params["user_id"]
+        return UJSONResponse(await get_user_posts(user_id))
