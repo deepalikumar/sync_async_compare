16a838eb33de1b96dee15d736f15a073befaf279
Deepali Kumar <d@acnt.me>
2020-02-11 14:04:10 +0530

    Explicit routing

--- fastapi_async/blog/views.py
+++ fastapi_async/blog/views.py
@@ -4,18 +4,29 @@ from fastapi import APIRouter, Depends
 
 from resources import get_session
 from .dal import get_user_posts, get_user_posts_sync
-from .schemas import Post
-
-router = APIRouter()
+from .schemas import PostSchema, PostORMSchema
 
 
-@router.get("/users/{user_id}/posts", response_model=List[Post])
 async def user_posts(user_id: int):
     posts = await get_user_posts(user_id)
     return posts
 
 
-@router.get("/users/{user_id}/posts/sync", response_model=List[Post])
 def user_posts_sync(user_id: int, session=Depends(get_session)):
     posts = get_user_posts_sync(session, user_id)
     return posts
+
+
+router = APIRouter()
+router.add_api_route(
+    "/users/{user_id}/posts",
+    user_posts,
+    response_model=List[PostSchema],
+    methods=("GET",),
+)
+router.add_api_route(
+    "/users/{user_id}/posts/sync",
+    user_posts_sync,
+    response_model=List[PostORMSchema],
+    methods=("GET",),
+)
